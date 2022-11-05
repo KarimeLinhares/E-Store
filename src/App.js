e93c7fab47c1e732de6b31c13ea213395a8a1a1b
KarimeLinhares <karime_muniz@hotmail.com>
2022-11-05 12:26:24 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import {Routes, Route} from 'react-router-dom';
+import Products from './components/Products';
 
+//caminho nos links internos
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/products' element={<Products/>} />
+      </Routes>
     </>
   );
 }
