5befaf6a27eb422591a60e8a0038d9df81165a9a
KarimeLinhares <karime_muniz@hotmail.com>
2022-11-05 13:08:57 -0300

    Home photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Products from './Products';
+import React from "react";
+import Products from "./Products";
 
 const Home = () => {
   return (
-    <div>
+    <div className="banner">
+      <div class="card bg-dark text-white border-0">
+        <img src="/assets/welcome.jpg" class="card-img" alt="background" height={600} />
+        <div class="card-img-overlay">
+          <div className="card-img-overlay d-flex align-items-end flex-column">
+            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON</h5>
+            <p class="card-text lead fs-2">Veja todos os nossos produtos</p>
+          </div>
+        </div>
+      </div>
       <Products></Products>
     </div>
   );
-}
+};
 
 export default Home;
